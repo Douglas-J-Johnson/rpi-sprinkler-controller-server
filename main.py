3f08edb66513c455d95fe1f0835e66429de77e07
Douglas-J-Johnson <douglas.j.johnson@gmail.com>
2021-08-30 02:10:32 +0000

    update variables to dictionaries, use the initial states from dictionary, add comments

--- main.py
+++ main.py
@@ -1,75 +1,35 @@
-#!/usr/bin/python
-# -*- coding:UTF-8 -*-
-
 from bottle import *
 import RPi.GPIO as GPIO
 import socket
 
-#GPIO Pins (BCM Numbering)
-Relay = [5, 6, 13, 16, 19, 20, 21, 26]
+# Specify GPIO pins using BCM numbering and their initial states
+relay_pins = {'CH1': 5, 'CH2': 6, 'CH3': 13, 'CH4': 16, 'CH5': 19, 'CH6': 20, 'CH7': 21, 'CH8': 26}
+relay_states = {'CH1': 1, 'CH2': 1, 'CH3': 1, 'CH4': 1, 'CH5': 1, 'CH6': 1, 'CH7': 1, 'CH8': 1}
+
+# Set mode to use BCM (Broadcom) pin numbering
 GPIO.setmode(GPIO.BCM)
 
-# ------------------------------
-# |Channel | RPI pin | BCM     |
-# ------------------------------
-# |CH1     | 29      | 5       |
-# |CH2     | 31      | 6       |
-# |CH3     | 33      | 13      |
-# |CH4     | 36      | 16      |
-# |CH5     | 35      | 19      |
-# |CH6     | 38      | 20      |
-# |CH7     | 40      | 21      |
-# |CH8     | 37      | 26      |
-# ------------------------------
+# For each GPIO pin set up the pin as an output pin and set pin to initial state
+for channel in relay_pins.keys():
+  GPIO.setup(relay_pins[channel], GPIO.OUT)
+  GPIO.output(relay_pins[channel], relay_states[channel])
 
-#All the relays
-Relay1 = 1
-Relay2 = 1
-Relay3 = 1
-Relay4 = 1
-Relay5 = 1
-Relay6 = 1
-Relay7 = 1
-Relay8 = 1
-
-for i in range(8):
-  GPIO.setup(Relay[i], GPIO.OUT)
-  GPIO.output(Relay[i], GPIO.HIGH)
-
+# Set up routes and functions
+# Get status of all relays
 @get("/relays")
-def index():
-#  global Relay1,Relay2,Relay3,Relay4,Relay5,Relay6,Relay7,Relay8
-   print("Get Relays")
+def get_relays():
+   global relay_states
 
+# Set status of all relays
 @post('/relays')
-def Relay_Control():
-  global Relay1,Relay2,Relay3,Relay4,Relay5,Relay6,Relay7,Relay8
+def set_relays():
+  global relay_states
 
-  Relay1 = request.POST.get('Relay1')
-  Relay2 = request.POST.get('Relay2')
-  Relay3 = request.POST.get('Relay3')
-  Relay4 = request.POST.get('Relay4')
-  Relay5 = request.POST.get('Relay5')
-  Relay6 = request.POST.get('Relay6')
-  Relay7 = request.POST.get('Relay7')
-  Relay8 = request.POST.get('Relay8')
-  print(Relay1)
-  print(Relay2)
-  print(Relay3)
-  print(Relay4)
-  print(Relay5)
-  print(Relay6)
-  print(Relay7)
-  print(Relay8)
-  GPIO.output(Relay[0], int(Relay1))
-  GPIO.output(Relay[1], int(Relay2))
-  GPIO.output(Relay[2], int(Relay3))
-  GPIO.output(Relay[3], int(Relay4))
-  GPIO.output(Relay[4], int(Relay5))
-  GPIO.output(Relay[5], int(Relay6))
-  GPIO.output(Relay[6], int(Relay7))
-  GPIO.output(Relay[7], int(Relay8))
+  for channel in relay_states.keys():
+    relay_states[channel] = int(request.POST.get(channel))
+    GPIO.output(relay_pins[channel], relay_states[channel])
 
+# Set up server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('8.8.8.8', 80))
 localhost = s.getsockname()[0]
