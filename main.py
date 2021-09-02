d5f711a631e8105bdc0b9b1b2878a378fd6abc6e
Douglas-J-Johnson <douglas.j.johnson@gmail.com>
2021-09-02 00:43:49 +0000

    udpates comment, debug, and return JSON Reponse of relay states

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from bottle import *
 import RPi.GPIO as GPIO
 import socket
+import json
 
 # Specify GPIO pins using BCM numbering and their initial states
+# --------------------------------------------------------------------------------------------------
 relay_pins = {'CH1': 5, 'CH2': 6, 'CH3': 13, 'CH4': 16, 'CH5': 19, 'CH6': 20, 'CH7': 21, 'CH8': 26}
 relay_states = {'CH1': 1, 'CH2': 1, 'CH3': 1, 'CH4': 1, 'CH5': 1, 'CH6': 1, 'CH7': 1, 'CH8': 1}
 
+# Set up Raspberry Pi Pins
+# --------------------------------------------------------------------------------------------------
 # Set mode to use BCM (Broadcom) pin numbering
 GPIO.setmode(GPIO.BCM)
 
@@ -15,10 +19,15 @@ for channel in relay_pins.keys():
   GPIO.output(relay_pins[channel], relay_states[channel])
 
 # Set up routes and functions
+# --------------------------------------------------------------------------------------------------
 # Get status of all relays
 @get("/relays")
 def get_relays():
-   global relay_states
+  global relay_states
+
+  response.headers['Content-Type'] = 'application/json'
+  print(json.dumps(relay_states))
+  return json.dumps(relay_states)
 
 # Set status of all relays
 @post('/relays')
@@ -26,10 +35,16 @@ def set_relays():
   global relay_states
 
   for channel in relay_states.keys():
+    print(channel + ' ' + request.POST.get(channel))
     relay_states[channel] = int(request.POST.get(channel))
     GPIO.output(relay_pins[channel], relay_states[channel])
 
+  response.headers['Content-Type'] = 'application/json'
+  print(json.dumps(relay_states))
+  return json.dumps(relay_states)
+
 # Set up server
+# --------------------------------------------------------------------------------------------------
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('8.8.8.8', 80))
 localhost = s.getsockname()[0]
